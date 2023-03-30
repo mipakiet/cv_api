cd5e1f18f526db09ed44cbb7cec1741b51beb40a
mipakiet <mikolajpakiet@gmail.com>
2023-03-30 04:08:03 +0200

    add draft code

--- main.py
+++ main.py
@@ -2,6 +2,31 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# employee
+
+# view  cv
+@app.route("/cv/", methods=['GET'])
+def cv():
+    pass
+
+# see and edit skill and technolgy name
+@app.route("/technology", methods=['GET', 'POST'])
+def technology():
+    pass
+
+# see and edit expiriance
+@app.route("/expiriance ", methods=['GET', 'POST'])
+def expiriance():
+    pass
+
+# staff and admin
+
+@app.route("/user/<username>/cv", methods=['GET'])
+def user_cv():
+    pass
+
+@app.route("/static/count", methods=['GET'])
+def user_cv():
+    technology = request.args.get('technology', None)
+    skill = request.args.get('skill', None)
+    pass
